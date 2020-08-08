382499f06ac0d5906fbe78f6c8b476c821bf953b
Tran Minh Hieu <tranminhhieu9901@gmail.com>
2020-08-08 17:15:07 +0700

    File Upload Database

--- models/book.js
+++ models/book.js
@@ -1,7 +1,7 @@
-const path = require('path');
+
 const mongoose = require('mongoose');
 
-const coverImageBasePath = 'uploads/bookCovers';
+
 
 const bookSchema = new mongoose.Schema({
     title: {
@@ -24,7 +24,11 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
         type: String,
         required: true
     },
@@ -36,12 +40,10 @@ const bookSchema = new mongoose.Schema({
 });
 
 bookSchema.virtual('coverImagePath').get(function() {
-    if(this.coverImageName != null) {
-        return path.join('/',coverImageBasePath, this.coverImageName);
-    } else {
-        console.log('Chac vua xoa me no roi T.T ');
+    if(this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,
+        ${this.coverImage.toString('base64')}`;
     }
 });
 
 module.exports = mongoose.model('Book',bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath;
